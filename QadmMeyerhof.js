806c7687d59a7a51e1247a0dd170a5402a69565d
ProgramsTest <dotaeswey@gmail.com>
2025-05-23 15:57:46 +0200

    Update QadmMeyerhof.js

--- QadmMeyerhof.js
+++ QadmMeyerhof.js
@@ -1,143 +1,203 @@
 class QadmMeyerhof {
-  constructor(Datos_Estratos, NF, Beta, Df, L, B) {
-    this.mDatos_Estratos = Datos_Estratos;
-    this.mNF = NF;
-    this.mBeta = Beta;
-    this.mDf = Df;
-    this.mL = L;
-    this.mB = B;
+  constructor(
+    Datos,
+    B_min, B_max, dB,
+    L, // L is now a single fixed value
+    DF_min, DF_max, dF,
+    Nfreatico,
+    Beta = 0
+  ) {
+    this.Datos = Datos;
+    this.Nfreatico = Nfreatico;
+    this.Peso_agua = 1;
+    this.Beta = Beta;
+  
+    this.B_data = this.range(B_min, B_max, dB);
+    this.L_data = Array(this.B_data.length).fill(L); // L is fixed
+    this.DF_data = this.range(DF_min, DF_max, dF);
+  
+    this.numB = this.B_data.length;
+    this.numDF = this.DF_data.length;
+  
+    this.initOutputMatrices();
 
-    this.initResultArrays();
-    this.Procesa_Cap();
+    this.calculate();
+  }
+  
+  range(min, max, step) {
+    if (min === max) return [min];
+    return Array.from({ length: Math.floor((max - min) / step) + 1 }, (_, i) => min + i * step);
+  }
+  
+
+  degToRad(deg) {
+    return deg * Math.PI / 180;
   }
 
-  initResultArrays() {
-    const rows = this.mDf.length;
-    const cols = this.mL.length;
-    const zeroMatrix = () => Array.from({ length: rows }, () => Array(cols).fill(0));
+  initOutputMatrices() {
+    const shape = () => Array.from({ length: this.numDF }, () => Array(this.numB).fill(0));
 
-    this.mQu_Resultados = zeroMatrix();
-    this.mQa_Resultados = zeroMatrix();
+    this.Qu = shape();
+    this.Qa = shape();
 
-    this.mNc_Resultados = zeroMatrix();
-    this.mNq_Resultados = zeroMatrix();
-    this.mNg_Resultados = zeroMatrix();
+    this.Nq = shape();
+    this.Nc = shape();
+    this.Ng = shape();
+
+    this.Fcs = shape();
+    this.Fqs = shape();
+    this.Fgs = shape();
+
+    this.Fcd = shape();
+    this.Fqd = shape();
+    this.Fgd = shape();
+
+    this.Fci = shape();
+    this.Fqi = shape();
+    this.Fgi = shape();
   }
 
-  Procesa_Cap() {
-    const Datos = this.mDatos_Estratos;
-    const Nfreatico = this.mNF;
-    const Beta = this.mBeta;
-    const DF = this.mDf;
-    const L_data = this.mL;
-    const B_data = this.mB;
-    const Peso_agua = 1;
+  calculate() {
+    const { Datos, Nfreatico, Peso_agua, B_data, L_data, DF_data } = this;
+    const numEstratos = Datos.length;
 
-    for (let i = 0; i < DF.length; i++) {
-      const Df = DF[i];
-      for (let j = 0; j < L_data.length; j++) {
-        const L = L_data[j];
+
+    for (let i = 0; i < this.numDF; i++) {
+      const Df = DF_data[i];
+
+      for (let j = 0; j < this.numB; j++) {
         const B = B_data[j];
+        const L = L_data[j];
 
-        let c = 0, phi = 0, Peso_especifico2 = 0, Peso_saturado2 = 0, Peso_sumergido2 = 0, ub = 0;
+        let c, phi, Peso_especifico2, Peso_saturado2, Peso_sumergido2, ub;
 
-        for (let k = 0; k < Datos.length; k++) {
+        for (let k = 0; k < numEstratos; k++) {
           const D0 = k === 0 ? 0 : Datos[k - 1][0];
           const D1 = Datos[k][0];
-          if (D1 > Df && D0 <= Df) {
+
+          if (D1 > Df && D0 < Df) {
             c = Datos[k][4];
             phi = Datos[k][3];
             Peso_especifico2 = Datos[k][1];
             Peso_saturado2 = Datos[k][2];
             Peso_sumergido2 = Peso_saturado2 - Peso_agua;
-            ub = k + 1;
+            ub = k;
             break;
           }
         }
 
-        let q = 0, ganma = 0;
+        let ganma, q = 0;
+
         if (Nfreatico <= Df) {
           ganma = Peso_sumergido2;
-          for (let l = 0; l < ub; l++) {
+          for (let l = 0; l <= ub; l++) {
             const ant = l === 0 ? 0 : Datos[l - 1][0];
             if (Datos[l][0] <= Nfreatico) {
               q += (Datos[l][0] - ant) * Datos[l][1];
             } else {
-              if (l === ub - 1) {
-                if (Datos[l - 1][0] > Nfreatico) {
-                  q += (Df - ant) * (Datos[l][2] - Peso_agua);
-                } else {
-                  q += (Nfreatico - ant) * Datos[l][1] + (Df - Nfreatico) * (Datos[l][2] - Peso_agua);
-                }
-              } else {
-                q += (Nfreatico - ant) * Datos[l][1] + (Datos[l][0] - Nfreatico) * (Datos[l][2] - Peso_agua);
-              }
+              q += (Nfreatico - ant) * Datos[l][1] + (Datos[l][0] - Nfreatico) * (Datos[l][2] - Peso_agua);
             }
+          }
+          if (Datos[ub][0] < Df) {
+            q += (Df - Datos[ub][0]) * (Datos[ub][2] - Peso_agua);
           }
         } else {
           const d = Nfreatico - Df;
-          ganma = d <= B
-            ? Peso_sumergido2 + (d / B) * (Peso_especifico2 - Peso_sumergido2)
-            : Peso_especifico2;
-
-          for (let m = 0; m < ub; m++) {
+          if (d <= B) {
+            const Peso_prima = Peso_sumergido2;
+            ganma = Peso_prima + (d / B) * (Peso_especifico2 - Peso_prima);
+          } else {
+            ganma = Peso_especifico2;
+          }
+          for (let m = 0; m <= ub; m++) {
             const ant = m === 0 ? 0 : Datos[m - 1][0];
-            const altura = m === ub - 1 ? Df - ant : Datos[m][0] - ant;
+            const altura = m === ub ? Df - ant : Datos[m][0] - ant;
             q += altura * Datos[m][1];
           }
         }
 
-        let Nc, Nq, Ng;
-        if (phi === 0) {
-          Nq = 1;
-          Nc = 5.14;
-          Ng = 0;
+        const phiRad = this.degToRad(phi);
+        const tanPhi = Math.tan(phiRad);
+        const sinPhi = Math.sin(phiRad);
+        const Nq = Math.pow(Math.tan(Math.PI / 4 + phiRad / 2), 2) * Math.exp(Math.PI * tanPhi);
+        const Nc = (Nq - 1) / tanPhi;
+        const Ng = (Nq - 1) * Math.tan(1.4 * phiRad);
+
+        const Fcs = 1 + (B * Nq) / (L * Nc);
+        const Fqs = 1 + (B * tanPhi) / L;
+        const Fgs = 1 - 0.4 * B / L;
+
+        let Fcd, Fqd;
+        if (Df / B <= 1) {
+          if (phi === 0) {
+            Fcd = 1 + 0.4 * Df / B;
+            Fqd = 1;
+          } else {
+            Fqd = 1 + 2 * tanPhi * Math.pow(1 - sinPhi, 2) * (Df / B);
+            Fcd = Fqd - (1 - Fqd) / (Nc * tanPhi);
+          }
         } else {
-          const phiRad = phi * Math.PI / 180;
-          Nq = Math.pow(Math.tan((Math.PI / 4) + (phiRad / 2)), 2) * Math.exp(Math.PI * Math.tan(phiRad));
-          Nc = (Nq - 1) / Math.tan(phiRad);
-          Ng = 2 * (Nq + 1) * Math.tan(phiRad);
+          if (phi === 0) {
+            Fcd = 1 + 0.4 * Math.atan(Df / B);
+            Fqd = 1;
+          } else {
+            Fqd = 1 + 2 * tanPhi * Math.pow(1 - sinPhi, 2) * Math.atan(Df / B);
+            Fcd = Fqd - (1 - Fqd) / (Nc * tanPhi);
+          }
         }
 
-        const qu = c * Nc + q * Nq + 0.5 * ganma * B * Ng;
+        const Fgd = 1;
+
+        const Fci = Math.pow(1 - this.Beta / 90, 2);
+        const Fqi = Fci;
+        const Fgi = 1 - this.Beta / phi;
+
+        const qu = c * Nc * Fcs * Fcd * Fci + q * Nq * Fqs * Fqd * Fqi + 0.5 * ganma * B * Ng * Fgs * Fgd * Fgi;
         const qadm = qu / 3;
 
-        this.mQu_Resultados[i][j] = qu;
-        this.mQa_Resultados[i][j] = qadm;
-        this.mNc_Resultados[i][j] = Nc;
-        this.mNq_Resultados[i][j] = Nq;
-        this.mNg_Resultados[i][j] = Ng;
+        console.log(L)
+
+        // Store results
+        this.Qu[i][j] = qu;
+        this.Qa[i][j] = qadm;
+
+        this.Nq[i][j] = Nq;
+        this.Nc[i][j] = Nc;
+        this.Ng[i][j] = Ng;
+
+        this.Fcs[i][j] = Fcs;
+        this.Fqs[i][j] = Fqs;
+        this.Fgs[i][j] = Fgs;
+
+        this.Fcd[i][j] = Fcd;
+        this.Fqd[i][j] = Fqd;
+        this.Fgd[i][j] = Fgd;
+
+        this.Fci[i][j] = Fci;
+        this.Fqi[i][j] = Fqi;
+        this.Fgi[i][j] = Fgi;
       }
     }
   }
 
-  // ✅ REQUIRED METHODS FOR main.js
-  getQuResultados() {
-    return this.mQu_Resultados;
-  }
-
-  getQaResultados() {
-    return this.mQa_Resultados;
-  }
-
-  // ✅ OPTIONAL METHODS to show factors in the table
-  getNc() {
-    return this.mNc_Resultados;
-  }
-
-  getNq() {
-    return this.mNq_Resultados;
-  }
-
-  getNg() {
-    return this.mNg_Resultados;
-  }
-
-  // Legacy method (optional to keep)
-  getResultados() {
+  getResults() {
     return {
-      Qu: this.mQu_Resultados,
-      Qa: this.mQa_Resultados
+      Qu: this.Qu,
+      Qa: this.Qa,
+      Nq: this.Nq,
+      Nc: this.Nc,
+      Ng: this.Ng,
+      Fcs: this.Fcs,
+      Fqs: this.Fqs,
+      Fgs: this.Fgs,
+      Fcd: this.Fcd,
+      Fqd: this.Fqd,
+      Fgd: this.Fgd,
+      Fci: this.Fci,
+      Fqi: this.Fqi,
+      Fgi: this.Fgi,
+      DF_data: this.DF_data,
+      B_data: this.B_data,
     };
   }
 }
